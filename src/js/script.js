219c827c5dd85925d5914310e4ac7245c6155b1a
chihiro <chanchi0707@gmail.com>
2019-07-07 16:40:13 +0900

    とりあえずpcのマークアップはこれでいい気がする

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,11 @@
 import { BASE_DIR } from '../constants.yml'
 import Sample from '@/lib/Sample';
 
-const sample = new Sample({
-    name: 'world'
-});
+const body = document.querySelector(body);
 
-document.querySelector('.wrapper').addEventListener('click', () => {
-    console.log(`hello, ${sample.name}. Base directory is ${BASE_DIR}.`);
-});
+window.onscroll = function()
+{
+    var scrollTop =
+        document.body.scrollTop || // IE、Firefox、Opera
+        document.body.scrollTop;              // Chrome、Safari
+}
